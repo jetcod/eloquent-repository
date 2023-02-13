fb65724cb9c78292f0e043a98385fb4c030bc689
Hamid Ghorashi <h.ghorashi@gmail.com>
2023-02-13 11:57:08 +0400

    Add console command to create repository class

---
+++ src/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Jetcod\LaravelRepository;
+
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+use Jetcod\LaravelRepository\Console\RepositoryMakeCommand;
+
+class ServiceProvider extends IlluminateServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                RepositoryMakeCommand::class,
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+    }
+}
