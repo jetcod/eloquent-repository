0f22c7bd846fd8a4a7009c67645ca30af946d1d8
Hamid Ghorashi <h.ghorashi@gmail.com>
2024-11-10 01:22:28 +0400

    Add avialbel repository methods to the contract

--- src/Eloquent/EloquentRepositoryInterface.php
+++ src/Eloquent/EloquentRepositoryInterface.php
@@ -2,6 +2,7 @@
 
 namespace Jetcod\LaravelRepository\Eloquent;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,11 +11,13 @@ use Illuminate\Database\Eloquent\Model;
  */
 interface EloquentRepositoryInterface
 {
+    public function query(): Builder;
+
     public function insert(array $rows): bool;
 
     public function create(array $attributes): Model;
 
-    public function delete(Model $model);
+    public function delete($model);
 
     public function destroy($ids): int;
 
@@ -24,6 +27,8 @@ interface EloquentRepositoryInterface
 
     public function find(int $id): ?Model;
 
+    public function findWithRelations(int $id, array $relations): ?Model;
+
     public function findOneBy(array $conditions, array $relations = []): ?Model;
 
     public function findBy(array $conditions, array $relations = [], bool $paginate = true, int $pageSize = 10);
@@ -31,4 +36,6 @@ interface EloquentRepositoryInterface
     public function findAll(): Collection;
 
     public function countBy(array $conditions): int;
+
+    public function with(array $relations): Builder;
 }
